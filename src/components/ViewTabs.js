3d08bac077714dd87762331ea2cfc43cd715696d
Oskari päivinen <osku_paivinen@hotmail.com>
2018-10-24 03:03:06 +0300

    viewtab placeholders

---
+++ src/components/ViewTabs.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Tab } from 'semantic-ui-react';
+
+import GraphBox from './GraphBox';
+import './ViewTabs.scss';
+
+const panes = [
+  {
+    menuItem: 'Tab 1',
+    render: () => (
+      <Tab.Pane attached={false}>
+        <GraphBox i="1" />
+      </Tab.Pane>
+    )
+  },
+  {
+    menuItem: 'Tab 2',
+    render: () => (
+      <Tab.Pane attached={false}>
+        <GraphBox i="2" />
+      </Tab.Pane>
+    )
+  },
+  {
+    menuItem: 'Tab 3',
+    render: () => (
+      <Tab.Pane attached={false}>
+        <GraphBox i="3" />
+      </Tab.Pane>
+    )
+  }
+];
+
+const ViewTabs = () => (
+  <Tab
+    className={'tabs'}
+    menu={{ secondary: true, pointing: true }}
+    panes={panes}
+  />
+);
+
+export default ViewTabs;
