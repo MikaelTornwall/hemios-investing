5fd1566c2c680e1438b0ff87fabd782e80718208
Oskari päivinen <osku_paivinen@hotmail.com>
2018-11-06 15:53:40 +0200

    moved parameter components inside the tabs

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import './App.scss';
 
 import HeaderBar from './components/HeaderBar';
 import ViewTabs from './components/ViewTabs';
-import ParameterAccordion from './components/ParameterAccordion';
 
 class App extends Component {
   render() {
     return (
       <>
         <HeaderBar />
-        <ParameterAccordion />
         <ViewTabs />
       </>
     );
--- src/components/ViewTabs.js
+++ src/components/ViewTabs.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Tab } from 'semantic-ui-react';
 
 import GraphCard from './GraphCard';
+import ParameterAccordion from './ParameterAccordion';
+
 import './ViewTabs.scss';
 
 const panes = [
@@ -9,6 +11,7 @@ const panes = [
     menuItem: 'Tab 1',
     render: () => (
       <Tab.Pane attached={false}>
+        <ParameterAccordion />
         <GraphCard i="1" />
       </Tab.Pane>
     )
@@ -17,6 +20,7 @@ const panes = [
     menuItem: 'Tab 2',
     render: () => (
       <Tab.Pane attached={false}>
+        <ParameterAccordion />
         <GraphCard i="2" />
       </Tab.Pane>
     )
@@ -25,6 +29,7 @@ const panes = [
     menuItem: 'Tab 3',
     render: () => (
       <Tab.Pane attached={false}>
+        <ParameterAccordion />
         <GraphCard i="3" />
       </Tab.Pane>
     )
