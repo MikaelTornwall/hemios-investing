c768dcb8c08de1cbd4b2698dc4f1ee7072c7f212
Helena Meissner <helena@aiven.io>
2018-11-07 09:32:19 +0200

    first version of landing page

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import { Icon, Header } from 'semantic-ui-react';
+import { Icon, Header, Button } from 'semantic-ui-react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { NavLink } from 'react-router-dom';
+
 import './HeaderBar.scss';
 
 const HeaderBar = () => {
   return (
     <>
       <div className={'header-bar'}>
-        <Icon className={'left'} name="area graph" size="huge" />
-        <Header className={'left'} as="h3">
-          Hemios Investing
-        </Header>
-        <Icon className={'right'} name="user circle" size="huge" />
+        <div className="leftSided">
+          <Icon name="area graph" size="huge" />
+          <Header as="h3">Hemios Investing</Header>
+        </div>
+        <div className="rightSided">
+          <NavLink to="/dashboard">
+            <Button className="primary">To Dashboard</Button>
+          </NavLink>
+          <Button className="basic primary">Login</Button>
+          <FontAwesomeIcon className="fa-icon" icon="user-circle" />
+        </div>
       </div>
     </>
   );
