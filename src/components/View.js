590cf836c42cacf632f3810659f0423f1361abf9
Oskari päivinen <oskari.paivinen@gmail.com>
2018-11-12 01:19:53 +0200

    connecting email to git with trivial changes

---
+++ src/components/View.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import {} from 'semantic-ui-react';
+
+import GraphCard from './GraphCard';
+import ParameterAccordionContainer from '../containers/ParameterAccordionContainer';
+
+export default class View extends Component {
+  state = {
+    selectedCompanies: [],
+    selectedKPIs: [],
+    selectedTimeInterval: []
+  };
+
+  updateCompanies = selectedCompanies => {
+    this.setState({
+      selectedCompanies: selectedCompanies
+    });
+  };
+
+  updateKPIs = selectedKPIs => {
+    this.setState({
+      selectedKPIs: selectedKPIs
+    });
+  };
+
+  updateTimeInterval = selectedTimeInterval => {
+    this.setState({
+      selectedTimeInterval: selectedTimeInterval
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <ParameterAccordionContainer
+          updateCompanies={this.updateCompanies}
+          updateKPIs={this.updateKPIs}
+          updateTimeInterval={this.updateTimeInterval}
+          tab={1}
+        />
+        <GraphCard i="1" />
+      </>
+    );
+  }
+}
